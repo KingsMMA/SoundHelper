51b74d0f15890af431369b28215ebfc44945b6aa
KingsDev <adking08@hotmail.com>
2023-07-04 22:03:06 +1000

    Properly prevent sounds from being played

--- src/main/java/dev/kingrabbit/soundhelper/mixin/SoundSystemMixin.java
+++ src/main/java/dev/kingrabbit/soundhelper/mixin/SoundSystemMixin.java
@@ -3,9 +3,12 @@ package dev.kingrabbit.soundhelper.mixin;
 import dev.kingrabbit.soundhelper.SoundHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.client.sound.SoundManager;
 import net.minecraft.client.sound.SoundSystem;
 import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,9 +16,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(SoundSystem.class)
 public class SoundSystemMixin {
 
-    @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("RETURN"), cancellable = true)
+    @Shadow @Final private SoundManager loader;
+
+    @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"), cancellable = true)
     public void play(SoundInstance sound, CallbackInfo cir) {
-        if (sound == null || sound.getSound() == null) return;
+        if (sound == null) return;
+        sound.getSoundSet(loader);
+        if (sound.getSound() == null) return;
         if (SoundHelper.isBlocked(sound)) {
             cir.cancel();
             return;
