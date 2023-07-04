1545f0ea87df667ca71129a29cffd96b34ef0acb
KingsDev <adking08@hotmail.com>
2023-07-04 20:21:42 +1000

    Fixed boilerplate code
    Thanks MCDev

---
+++ src/main/java/dev/kingrabbit/soundhelper/SoundHelper.java
@@ -0,0 +1,15 @@
+package dev.kingrabbit.soundhelper;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class SoundHelper implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+    
+}
